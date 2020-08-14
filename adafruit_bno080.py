bbc60f793623a6fd51358976b0894b0c73edcb20
siddacious <nospam187+github@gmail.com>
2020-08-14 16:46:14 -0700

    added classes for header and packet

--- adafruit_bno080.py
+++ adafruit_bno080.py
@@ -28,7 +28,8 @@ __version__ = "0.0.0-auto.0"
 __repo__ = "https://github.com/adafruit/Adafruit_CircuitPython_BNO080.git"
 
 from struct import unpack_from, pack_into
-from time import sleep
+from collections import namedtuple
+from time import sleep, monotonic
 from micropython import const
 import adafruit_bus_device.i2c_device as i2c_device
 
@@ -39,15 +40,115 @@ _BNO_CHANNEL_INPUT_SENSOR_REPORTS = const(3)
 _BNO_CHANNEL_WAKE_INPUT_SENSOR_REPORTS = const(4)
 _BNO_CHANNEL_GYRO_ROTATION_VECTOR = const(5)
 
-_BNO080_CMD_RESET = const(0x01)
+_BNO_CMD_GET_FEATURE_REQUEST = const(0xFE)
+_BNO_CMD_SET_FEATURE_COMMAND = const(0xFD)
+_BNO_CMD_GET_FEATURE_RESPONSE = const(0xFC)
 
-_BNO080_DEFAULT_ADDRESS = const(0x4A)
-
+_BNO_CMD_BASE_TIMESTAMP = const(0xFB)
+_BNO_CMD_TIMESTAMP_REBASE = const(0xFA)
+########## TODO ##########
+_BNO_CMD_PRODUCT_ID_REQUEST = const(0xF9)
+_BNO_CMD_PRODUCT_ID_RESPONSE = const(0xF8)
 
 _SHTP_REPORT_PRODUCT_ID_RESPONSE = const(0xF8)
 _SHTP_REPORT_PRODUCT_ID_REQUEST = const(0xF9)
+##################
+
+_BNO_CMD_FRS_WRITE_REQUEST = const(0xF7)
+_BNO_CMD_FRS_WRITE_DATA = const(0xF6)
+_BNO_CMD_FRS_WRITE_RESPONSE = const(0xF5)
+
+_BNO_CMD_FRS_READ_REQUEST = const(0xF4)
+_BNO_CMD_FRS_READ_RESPONSE = const(0xF3)
+
+_BNO_CMD_COMMAND_REQUEST = const(0xF2)
+_BNO_CMD_COMMAND_RESPONSE = const(0xF1)
+
+_BNO_REPORT_ACCELEROMETER = const(0x01)
+_BNO_REPORT_GYROSCOPE = const(0x02)
+_BNO_REPORT_MAGNETIC_FIELD = const(0x03)
+_BNO_REPORT_LINEAR_ACCELERATION = const(0x04)
+_BNO_REPORT_ROTATION_VECTOR = const(0x05)
+_BNO_REPORT_GRAVITY = const(0x06)
+_BNO_REPORT_UNCALIBRATED_GYROSCOPE = const(0x07)
+_BNO_REPORT_GAME_ROTATION_VECTOR = const(0x08)
+_BNO_REPORT_GEOMAGNETIC_ROTATION_VECTOR = const(0x09)
+_BNO_REPORT_PRESSURE = const(0x0A)
+_BNO_REPORT_AMBIENT_LIGHT = const(0x0B)
+_BNO_REPORT_HUMIDITY = const(0x0C)
+_BNO_REPORT_PROXIMITY = const(0x0D)
+_BNO_REPORT_TEMPERATURE = const(0x0E)
+_BNO_REPORT_UNCALIBRATED_MAGNETIC_FIELD = const(0x0F)
+_BNO_REPORT_TAP_DETECTOR = const(0x10)
+_BNO_REPORT_STEP_COUNTER = const(0x11)
+_BNO_REPORT_SIGNIFICANT_MOTION = const(0x12)
+_BNO_REPORT_STABILITY_CLASSIFIER = const(0x13)
+_BNO_REPORT_RAW_ACCELEROMETER = const(0x14)
+_BNO_REPORT_RAW_GYROSCOPE = const(0x15)
+_BNO_REPORT_RAW_MAGNETOMETER = const(0x16)
+_BNO_REPORT_SAR = const(0x17)
+_BNO_REPORT_STEP_DETECTOR = const(0x18)
+_BNO_REPORT_SHAKE_DETECTOR = const(0x19)
+_BNO_REPORT_FLIP_DETECTOR = const(0x1A)
+_BNO_REPORT_PICKUP_DETECTOR = const(0x1B)
+_BNO_REPORT_STABILITY_DETECTOR = const(0x1C)
+_BNO_REPORT_PERSONAL_ACTIVITY_CLASSIFIER = const(0x1E)
+_BNO_REPORT_SLEEP_DETECTOR = const(0x1F)
+_BNO_REPORT_TILT_DETECTOR = const(0x20)
+_BNO_REPORT_POCKET_DETECTOR = const(0x21)
+_BNO_REPORT_CIRCLE_DETECTOR = const(0x22)
+_BNO_REPORT_HEART_RATE_MONITOR = const(0x23)
+_BNO_REPORT_ARVR_STABILIZED_ROTATION_VECTOR = const(0x28)
+_BNO_REPORT_ARVR_STABILIZED_GAME_ROTATION_VECTOR = const(0x29)
+
+_QUAT_REPORT_INTERVAL = const(50000)  # in microseconds = 50ms
+_QUAT_READ_TIMEOUT = 1.000  # timeout in seconds
+_BNO080_CMD_RESET = const(0x01)
+
+_BNO080_DEFAULT_ADDRESS = const(0x4A)
+_BNO_HEADER_LEN = const(4)
+
 
 _DATA_BUFFER_SIZE = const(512)  # data buffer size. obviously eats ram
+Quaternion = namedtuple("Quaternion", ["i", "j", "k", "real", "accuracy",],)
+PacketHeader = namedtuple(
+    "PacketHeader", ["data_length", "channel_number", "sequence_number",],
+)
+
+
+def _elapsed(start_time):
+    return monotonic() - start_time
+
+
+def _get_header(packet_bytes):
+    packet_byte_count = unpack_from("<H", packet_bytes)[0]
+    packet_byte_count &= ~0x8000
+    channel_number = unpack_from("<B", packet_bytes, offset=2)[0]
+    sequence_number = unpack_from("<B", packet_bytes, offset=3)[0]
+    header = PacketHeader(packet_byte_count - 4, channel_number, sequence_number)
+    return header
+
+
+class Packet:
+    """A class representing a Hillcrest Laboratory **Sensor Hub Transport
+Protocol** packet"""
+
+    def __init__(self, packet_bytes):
+        self.header = _get_header(packet_bytes)
+        data_end_index = self.header.data_length + _BNO_HEADER_LEN
+        self.data = packet_bytes[_BNO_HEADER_LEN:data_end_index]
+
+    def __str__(self):
+        outstr = ""
+        outstr += "HEADER:"
+
+        outstr += "\tLen: %d\n" % (self.header.data_length)
+        outstr += "\tChannel: %s\n" % self.header.channel_number
+        outstr += "\tSequence number: %s\n" % self.header.sequence_number
+        outstr += "\nData:"
+        for _idx, _byte in enumerate(self.data):
+            outstr += "\t[%0.2d] %x\n" % (_idx, _byte)
+        return outstr
 
 
 class BNO080:
@@ -88,6 +189,39 @@ class BNO080:
             print("Again reading packet")
             data_read = self._read_packet()
             self._dbg("data read:", data_read)
+
+    # Constructs a report  to set a feature
+    # later: class-ify
+    # def _set_feature_report(self, feature_id):
+    @property
+    def rotation_vector(self):
+        """A quaternion representing the current rotation vector"""
+        # create and send a packet to enable the quaternion data
+        set_feature_report = bytearray(17)
+        set_feature_report[0] = _BNO_CMD_SET_FEATURE_COMMAND
+        set_feature_report[1] = _BNO_REPORT_ROTATION_VECTOR
+        pack_into("<I", set_feature_report, 5, _QUAT_REPORT_INTERVAL)
+
+        self._send_packet(_BNO_CHANNEL_CONTROL, set_feature_report)
+
+        quat_i = 0.0
+        quat_j = 0.0
+        quat_k = 0.0
+        quat_real = 0.0
+        quat_accuracy = 0.0
+
+        # receive packets, and dump until you get a quat packet
+        start_time = monotonic()
+        while _elapsed(start_time) < _QUAT_READ_TIMEOUT:
+            data_was_read = self._read_packet()
+            if not data_was_read:
+                break
+            print("Got a packet")
+            new_packet = Packet(self._data_buffer)
+            print(new_packet)
+
+        print("returning blank quaternion")
+        return Quaternion(quat_i, quat_j, quat_k, quat_real, quat_accuracy)
 
     def _check_id(self):
         print("Checking ID:")
@@ -233,6 +367,7 @@ class BNO080:
 
     # Useful features to possibly include:
     # re-map orientation "FRS record (0x2D3E)"
+    # data timestamp
     # DFU Support
     # Angular velocity
     # • Angular position (quaternion)
--- examples/bno080_simpletest.py
+++ examples/bno080_simpletest.py
@@ -1,12 +1,13 @@
 # SPDX-FileCopyrightText: 2017 Scott Shawcroft, written for Adafruit Industries
 #
 # SPDX-License-Identifier: Unlicense
+from time import sleep
 import board
 import busio
 import adafruit_bno080
 
 i2c = busio.I2C(board.SCL, board.SDA)
-bno = adafruit_bno080.BNO080(i2c)
+bno = adafruit_bno080.BNO080(i2c, debug=True)
 
 while True:
     quat = bno.rotation_vector  # pylint:disable=no-member
@@ -15,3 +16,4 @@ while True:
         "I: %0.3f J: %0.3f K: %0.3f Real: %0.3f Accuracy: %0.3f"
         % (quat.i, quat.j, quat.k, quat.real, quat.accuracy)
     )
+    sleep(0.5)
